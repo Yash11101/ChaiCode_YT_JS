0426601b6eaeda69318a764f988be1b1c2533dd8
Yash11101 <yashvpatil290906@gmail.com>
2026-06-22 18:39:57 +0530

    Added Day_21 file

---
+++ Day_21/async_code_notes.js
@@ -0,0 +1,13 @@
+/*
+JS is synchronous lang
+JS is single threaded / hence  relatively slow
+
+blocking code-->blocks flow of code // Stops execution until task finishes
+Non-blocking code-->doesnot blocks flow of code // Doesn’t stop execution
+
+JS executes program line by line
+each operation waits for previous to finish.
+
+web APIs handle asunc tasks-->it stores completed async task in task queue-->then moved to call stack when its empty
+
+*/
